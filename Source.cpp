1d9c20e5f9dd04be978dc69f09702bda46394571
Otter <ignatovad2006@gmail.com>
2026-01-30 13:00:44 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,52 +1,53 @@
 ﻿#include <iostream>
 
+
 int main() {
     setlocale(LC_CTYPE, "rus");
 
-    //задача 1
-    int number;
-    int sum = 0;
+    // задача 1
+    int number1;
+    int sum1 = 0;
 
     while (true) {
         std::cout << "Введите целое число или число '0', чтобы закончить:\n";
-        std::cin >> number;
+        std::cin >> number1;
 
-        if (number == 0) {
-            break;  
+        if (number1 == 0) {
+            break;
         }
 
-        sum += number;  
+        sum1 += number1;
     }
 
-    std::cout << "Сумма: " << sum << std::endl;
+    std::cout << "Сумма: " << sum1 << std::endl;
 
-    //задача 2
-    int number;
+    // задача 2
+    int number2;
     std::cout << "Введите целое число:\n";
-    std::cin >> number;
+    std::cin >> number2;
 
-    int temp = number;
+    int temp = number2;
     if (temp < 0) {
-        temp = -temp;  
+        temp = -temp;
     }
 
-    int sum = 0;
+    int sum2 = 0;
 
     while (temp > 0) {
-        int digit = temp % 10;  
-        sum += digit;           
-        temp /= 10;             
+        int digit = temp % 10;
+        sum2 += digit;
+        temp /= 10;
     }
 
-    std::cout << "Сумма цифр: " << sum << std::endl;
+    std::cout << "Сумма цифр: " << sum2 << std::endl;
 
-    //задача 3
-    int number;
+    // задача 3
+    int number3;
     std::cout << "Введите целое число:\n";
-    std::cin >> number;
+    std::cin >> number3;
 
     for (int i = 1; i <= 10; i++) {
-        std::cout << number << " x " << i << " = " << number * i << std::endl;
+        std::cout << number3 << " x " << i << " = " << number3 * i << std::endl;
     }
 
     return EXIT_SUCCESS;
